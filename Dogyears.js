beba87b74512a84a7556114ccfe3726e0a624839
Nukri Khutsishvili <158152732+NukReal@users.noreply.github.com>
2024-02-17 13:53:34 +0400

    Add files via upload

---
+++ Dogyears.js
@@ -0,0 +1,12 @@
+// so here I write my age
+const myAge = 29;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(laterYears)
+myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Nukri'.toLocaleLowerCase
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
